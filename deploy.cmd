2fc562c503d194a3916023ef7d72a518fa2063f2
zs5460 <zs5460@gmail.com>
2018-06-05 09:43:31 +0800

    update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,8 @@
 @echo off
-if exist watermark.zip (del watermark.zip)
-if exist watermark.exe (del watermark.exe)
-go build
-upx watermark.exe
-"c:\program files\7-zip\7z.exe" a watermark.zip watermark.exe watermark.png readme.txt sky.jpg
+if exist dist/watermark.zip (del dist/watermark.zip)
+if exist dist/watermark.exe (del dist/watermark.exe)
+go build -o dist/watermark.exe
+bft -page readme.md dist/readme.htm
+upx dist/watermark.exe
+cd dist
+"c:\program files\7-zip\7z.exe" a watermark.zip watermark.exe watermark.png readme.htm sky.jpg
